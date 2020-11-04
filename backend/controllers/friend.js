e0236260317d0260e094500049695959fe492c13
pongpol ounraun <ounraun.p@gmail.com>
2020-11-04 11:52:49 +0700

    update friends

---
+++ backend/controllers/friend.js
@@ -0,0 +1,24 @@
+const db = require('../models');
+
+const addFriend = (req,res) => {
+
+}
+
+const acceptFriend = (req,res) => {
+
+}
+
+const deleteFriend = (req,res) => {
+
+}
+
+const getAllFriend = (req,res) => {
+
+}
+
+module.exports = {
+    addFriend,
+    acceptFriend,
+    deleteFriend,
+    getAllFriend
+};
