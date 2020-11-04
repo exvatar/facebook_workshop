8c2a5637889bbe4d1ef3fd539a8c0b5fe1698d22
pongpol ounraun <ounraun.p@gmail.com>
2020-11-04 11:33:05 +0700

    update editPost

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -4,14 +4,14 @@ const createPost = async (req, res) => {
     const { caption, picture_url, createdAt, updatedAt } = req.body;
     const user = await db.User.findOne({ where: { id: req.user.id } });
     if (user) {
-        const newPost = await db.Post.create({ 
-            caption, 
-            picture_url, 
-            createdAt, 
-            updatedAt, 
-            user_id: user.id 
+        const newPost = await db.Post.create({
+            caption,
+            picture_url,
+            createdAt,
+            updatedAt,
+            user_id: user.id
         })
-        res.status(200).send({message:"Create Success", data: newPost})
+        res.status(200).send({ message: "Create Success", data: newPost })
     } else {
         res.status(400).send("Not found user ID");
     }
@@ -22,8 +22,15 @@ const deletePost = (req, res) => {
 
 };
 
-const editPost = (req, res) => {
-
+const editPost = async (req, res) => {
+    const targetPost = await db.Post.findOne({ where: { id: req.params.id, user_id: req.user.id } });
+    const { caption, picture_url } = req.body;
+    if (targetPost) {
+        await targetPost.update({ caption, picture_url })
+        res.status(200).send({ message: "Update success" })
+    } else {
+        res.status(400).send("Not found your post")
+    }
 };
 
 const getAllMyPost = (req, res) => {
